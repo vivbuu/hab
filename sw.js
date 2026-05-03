9d1cb4ef07da3c90b0ed653623a6e1ce04064f75
vivbuu <vipcompany8@ya.ru>
2026-05-03 11:01:49 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', function(e) {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(fetch(e.request).catch(function() {
+        return caches.match(e.request);
+    }));
+});
